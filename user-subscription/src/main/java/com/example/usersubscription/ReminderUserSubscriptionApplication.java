eacb16e45c12bf8bdab2353e62f55fcdeeee5c23
Meruem-Sudo <lastillasimone@gmail.com>
2025-11-06 17:11:35 +0100

    Iniziato progetto per reminder subscription

---
+++ user-subscription/src/main/java/com/example/usersubscription/ReminderUserSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderUserSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderUserSubscriptionApplication.class, args);
+	}
+
+}
---
+++ user-subscription/src/test/java/com/example/usersubscription/ReminderUserSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderUserSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
