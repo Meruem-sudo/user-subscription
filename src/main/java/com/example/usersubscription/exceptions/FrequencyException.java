b4bc4b3abd0ee1293a5ed88aabace4452d4da892
Meruem-Sudo <lastillasimone@gmail.com>
2025-12-08 12:10:47 +0100

    Aggiustata data di pagamento che si vede nella pagina home

---
+++ src/main/java/com/example/usersubscription/exceptions/FrequencyException.java
@@ -0,0 +1,7 @@
+package com.example.usersubscription.exceptions;
+
+public class FrequencyException extends RuntimeException {
+    public FrequencyException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/usersubscription/services/SubscriptionService.java
+++ src/main/java/com/example/usersubscription/services/SubscriptionService.java
@@ -1,7 +1,9 @@
 package com.example.usersubscription.services;
 
+import com.example.usersubscription.entities.Frequency;
 import com.example.usersubscription.entities.Subscription;
 import com.example.usersubscription.entities.User;
+import com.example.usersubscription.exceptions.FrequencyException;
 import com.example.usersubscription.repositories.SubscriptionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,13 @@ public class SubscriptionService {
 
     public Subscription addSubscription(Subscription subscription)
     {
+        switch (subscription.getFrequency())
+        {
+            case WEEKLY -> subscription.setEndDate(subscription.getStartDate().plusWeeks(1));
+            case MONTHLY -> subscription.setEndDate(subscription.getStartDate().plusMonths(1));
+            case YEARLY -> subscription.setEndDate(subscription.getStartDate().plusYears(1));
+            default -> throw new FrequencyException("Frequenza non rilevata");
+        }
         return subscriptionRepo.save(subscription);
     }
 
